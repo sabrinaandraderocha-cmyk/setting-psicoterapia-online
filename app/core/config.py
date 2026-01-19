ee382eeeb0aae2daeb24e3579ef38db6065e5d04
sabrinaandraderocha-cmyk <sabrina.andrade.rocha@gmail.com>
2026-01-19 18:23:31 -0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,6 @@ class Settings(BaseModel):
     app_name: str = os.getenv("APP_NAME", "Setting")
     secret_key: str = os.getenv("SECRET_KEY", "change-me-please")
     admin_username: str = os.getenv("ADMIN_USERNAME", "admin")
-    admin_password: str = os.getenv("ADMIN_PASSWORD", "admin")
+    admin_password: str = os.getenv("ADMIN_PASSWORD", "setting@2026!")
 
 settings = Settings()
